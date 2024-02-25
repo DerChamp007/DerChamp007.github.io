5f808128613793a42d2757714a6db9844196af69
srand <randip.singh@edu.bib.de>
2024-02-25 19:25:40 +0100

    main

--- Projekt/gamingpc-script.js
+++ Projekt/gamingpc-script.js
@@ -21,3 +21,15 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+const slider = document.querySelector(".image-comparison .slider");
+const beforeImage = document.querySelector(".image-comparison .before-image");
+const sliderLine = document.querySelector(".image-comparison .slider-line");
+const sliderIcon = document.querySelector(".image-comparison .slider-icon");
+
+slider.addEventListener("input", e =>{
+    let sliderValue = e.target.value + "%";
+
+    beforeImage.style.width = sliderValue;
+    sliderLine.style.left = sliderValue;
+    sliderIcon.style.left = sliderValue;
+});
